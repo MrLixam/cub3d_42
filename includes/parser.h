9beafa2229ae15d48186fe75523dbcf8cbf0b762
Liam VINCENT <lixam.vincent@gmail.com>
2024-02-08 09:18:57 +0100

    work on parsing and indexing

--- includes/parser.h
+++ includes/parser.h
@@ -6,7 +6,7 @@
 /*   By: lvincent <lvincent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 06:02:30 by lvincent          #+#    #+#             */
-/*   Updated: 2024/02/01 22:07:23 by lvincent         ###   ########.fr       */
+/*   Updated: 2024/02/08 08:43:47 by lvincent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@
 
 int	name_parser(char *name, char *ext);
 int	parse_extern(char *name, char *ext);
-int	is_valid_file(char *name);
+int	not_valid_file(char *name);
 
 #endif
--- src/parser/parser_interior.c
+++ src/parser/parser_interior.c
@@ -6,7 +6,7 @@
 /*   By: lvincent <lvincent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 00:11:04 by lvincent          #+#    #+#             */
-/*   Updated: 2024/02/05 14:53:58 by lvincent         ###   ########.fr       */
+/*   Updated: 2024/02/08 09:00:13 by lvincent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,21 +59,52 @@ int	base_tests(char *path)
 	return (0);
 }
 
-int	index_paths(char **file)
+int	index_path(char *line, char **storage)
 {
-	int	i;
+	char	*path;
+	int		start;
 
-	i = 0;
-	while (1)
+	start = 2;
+	while (ft_isspace(line[start]))
+		start++;
+	path = ft_substr(line, start, ft_strlen(line));
+	if (!path)
+		ft_error(NULL, "allocation error");
+	if (!path)
+		return (1);
+	if (name_parser(path, ".xpm"))
 	{
-		if (ft_strlen(file[i]) < 3)
-		{
-			i++;
-			continue;
-		}
-		if (is_in_set("NSEW", file[i][0]))
-		
+		ft_error(path, "Invalid file type, use .xpm only");
+		return (1);
 	}
+	if (not_valid_file(path))
+	{
+		ft_perror(path);
+		return (1);
+	}
+	*storage = path;
+	return (0);
+}
+
+int	is_path(char *line)
+{
+	int	rv;
+
+
+	rv = 0;
+	if (ft_strlen(line) < 3)
+		return (1);
+	if ((line[0] == 'N' && line[1] == 'O') && ft_isspace(line[2]))
+		rv = index_path(line, 1);
+	else if ((line[0] == 'S' && line[1] == 'O') && ft_isspace(line[2]))
+		rv = index_path(line, 2);
+	else if ((line[0] == 'W' && line[1] == 'E') && ft_isspace(line[2]))
+		rv = index_path(line, 3);
+	else if ((line[0] == 'E' && line[1] == 'A') && ft_isspace(line[2]))
+		rv = index_path(line, 4);
+	else
+		return (1);
+	return (rv);
 }
 
 int	valid_config(char *path)

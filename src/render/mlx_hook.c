9c656cb13d15e420237af7e168d31afe10a7cf54
Liam VINCENT <lixam.vincent@gmail.com>
2024-02-24 07:13:20 +0100

    updated moving function

--- src/render/mlx_hook.c
+++ src/render/mlx_hook.c
@@ -6,7 +6,7 @@
 /*   By: lvincent <lvincent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 16:06:23 by r                 #+#    #+#             */
-/*   Updated: 2024/02/24 06:28:31 by lvincent         ###   ########.fr       */
+/*   Updated: 2024/02/24 07:12:58 by lvincent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,47 +14,71 @@
 
 static void	update_position_x(t_game *game, float x)
 {
-	if (game->player.sub_x + 4 * x > RES)
+	float	new_sub_x;
+	int		tile_shift;
+
+	new_sub_x = game->player.sub_x + 4 * x;
+	tile_shift = 0;
+	if (new_sub_x > RES)
 	{
-		if (game->map[game->player.y][game->player.x + 1] != '1')
-		{
-			game->player.sub_x = 0;
-			game->player.x++;
-		}
+		tile_shift = 1;
+		new_sub_x = fmod(new_sub_x, RES);
 	}
-	else if (game->player.sub_x + 4 * x < 1)
+	else if (new_sub_x < 0)
 	{
-		if (game->map[game->player.y][game->player.x - 1] != '1')
-		{
-			game->player.sub_x = RES;
-			game->player.x--;
-		}
+		tile_shift = -1;
+		new_sub_x = RES - fmod(-new_sub_x, RES);
 	}
-	else
-		game->player.sub_x += 4 * x;
+	if (game->map[game->player.y][game->player.x + tile_shift] == '1')
+		return ;
+	game->player.sub_x = new_sub_x;
+	game->player.x += tile_shift;
 }
 
 static void	update_position_y(t_game *game, float y)
 {
+	float	new_sub_y;
+	int		tile_shift;
+
+	new_sub_y = game->player.sub_y + 4 * y;
+	tile_shift = 0;
+	if (new_sub_y > RES)
+	{
+		tile_shift = 1;
+		new_sub_y = fmod(new_sub_y, RES);
+	}
+	else if (new_sub_y < 0)
+	{
+		tile_shift = -1;
+		new_sub_y = RES - fmod(-new_sub_y, RES);
+	}
+	if (game->map[game->player.y + tile_shift][game->player.x] == '1')
+		return ;
+	game->player.sub_y = new_sub_y;
+	game->player.y += tile_shift;
+}
+
+/*	printf("y : %f\n", game->player.sub_y + 4 * y);
 	if (game->player.sub_y + 4 * y > RES)
 	{
+		printf("Y TRUE UP\n");
 		if (game->map[game->player.y + 1][game->player.x] != '1')
 		{
-			game->player.sub_y = 0;
+			game->player.sub_y = 2;
 			game->player.y++;
 		}
 	}
 	else if (game->player.sub_y + 4 * y < 1)
 	{
+		printf("Y TRUE DOWN\n");
 		if (game->map[game->player.y - 1][game->player.x] != '1')
 		{
-			game->player.sub_y = RES;
+			game->player.sub_y = RES - 1;
 			game->player.y--;
 		}
 	}
 	else
-		game->player.sub_y += 4 * y;
-}
+		game->player.sub_y += 4 * y;*/
 
 static int	player_action(t_game *game, float x, float y)
 {
